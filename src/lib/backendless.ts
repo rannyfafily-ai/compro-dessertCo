0366379e4834df025c3e82144f5b43bfe3ef01d5
rannyfafily-ai <rannyfafily@gmail.com>
2026-03-09 20:20:36 +0700

    update image

--- src/lib/backendless.ts
+++ src/lib/backendless.ts
@@ -3,12 +3,9 @@ import Backendless from "backendless";
 const APP_ID = process.env.NEXT_PUBLIC_BACKENDLESS_APP_ID;
 const API_KEY = process.env.NEXT_PUBLIC_BACKENDLESS_API_KEY;
 
-if (!APP_ID || !API_KEY) {
-  throw new Error(
-    "Backendless App ID atau API Key tidak ditemukan di environment variables."
-  );
+// Pastikan inisialisasi HANYA berjalan jika kunci ada
+if (APP_ID && API_KEY) {
+    Backendless.initApp(APP_ID, API_KEY);
 }
-
-Backendless.initApp(APP_ID, API_KEY);
 
 export default Backendless;
